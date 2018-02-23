9cb7cd3c561cf01074f2980c017ade8ad91915e3
Cristina Rivera <33240525+clr3@users.noreply.github.com>
2018-02-23 10:30:38 +0000

    Update laser_subscriber.py

--- scripts/laser_subscriber.py
+++ scripts/laser_subscriber.py
@@ -4,23 +4,42 @@ import rospy
 from sensor_msgs.msg import LaserScan
 
 #This class recieves data from the scanner
+# Angles are given in radians by sensor_msgs.LaserScan
 # 
-# 
+# For the robot in gazebo:
+# angle_min: -1.57079994678
+# angle_max: 1.57079994678
+# angle_increment: 0.00436940183863.
+# range_max = 30.0
+#
 
 class LaserScanReader():
-    def __init__(self, x_goal, y_goal):
-        self._topic_name = '/kobuki/laser/scan'
-        self._sub = rospy.Subscriber(self._topic_name, LaserScan, self.topic_callback)        
+    def __init__(self, x_goal, y_goal, topic_name = '/scan'):		#'/scan' is the topic that publisher LaserScan
+        self._topic_name = topic_name
+        self._sub = rospy.Subscriber(self._topic_name, LaserScan, self.topic_callback(msg)) # !!!topic_callback needs an argument (msg)   
 	self._threshold = 1		
 	self._x_goal = x_goal
 	self._y_goal = y_goal
+	#Initialize an ampty array to copy LaserScan.ranges[]
+	self._scanner_arrary = msg.ranges
+	#This array will hold values for every 10 deg:
+	#Degrees are measured from the right, anticlockwise
+	self._anlges_array = [0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180]	
+					
         
     def topic_callback(self, msg):
+	#Make a copy of laser array:
+	self._scanner_arrary = msg.ranges
 	#Check range of laser array:
-	
-	#Record values for every angle 
+	self._arr_size = len(self._scanner_array)
  	return 
 	
+	#Will create an array wich containt laser scan values at various degrees
+    def create_angles_array(self):
+	to
+
+
+
     def shutdownhook():
         global ctrl_c
         print "shutdown time!"
@@ -32,7 +51,7 @@ class LaserScanReader():
 
 if __name__ == "__main__":
     rospy.init_node('laserscan_sub_node')		#Creating the node
-    laserscan_sub_object = LaserScanReader()
+    laserscan_sub_object = LaserScanReader(0.0,0.0)	#Initialize with the values for the goal
     rospy.loginfo(laserscan_sub_object.topic_callback())  #Log 
     rate = rospy.Rate(0.5)
 
