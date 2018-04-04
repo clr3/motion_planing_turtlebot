e577a4b7250a3f5ce6813770be2ba69daa64da8b
Cristina Rivera <33240525+clr3@users.noreply.github.com>
2018-04-04 15:16:59 +0000

    Update calculte_direction.py

--- scripts/calculte_direction.py
+++ scripts/calculte_direction.py
@@ -6,122 +6,90 @@ import rospy
 # -From obstacles
 # -From the goal
 #
-# It calculates the best direction to move next
+#It calculates the best direction to move next. Requires infromation from /laser_scan and /odom .
 #
-# Assuming the values recieved are based on 180 deg view
+# Assuming the values recieved are based on 180 deg view. 
 #
-# Values optained from this class must be dampened before applying it to the robot
+# Values obtained from this class must be dampened before applying it to the robot. 
 #
 
 class CalculateDirection():
-    def __init__(self, x_goal, y_goal):
-
-	self._x_goal = x_goal
-	self._y_goal = y_goal	
-	self._goal_angle = 0.0
-
-	self._deltax_goal = 0
-	self._deltay_goal = 0
-	#The attracting force will be grater outside the threshho
-	self._goal_threshhold = 10 		
-	self._goal_radius = 2
-
+    def __init__(self):
+	self._dx = 0.0
+	self._dy = 0.0
+	self._distance = 0.0
+	#These arrays hold values from laserscan:
 	self._x_components = []
 	self._y_components = []
-	self._forces_angle = []
+	self._angles = []
 
 	self._deltax_components = []
 	self._deltay_components = []
-	#If an object is this close, the repelling force will be bigger
-	self._objs_threshold = 5
-	self._obj_min_distance = 0.8
 
+	self._obj_effect_radius = 5	#Repulsive force doesnt't exist outside it's threshhold
+	self._obj_min_distance = 0.8	#Repulsive force will be infinite if the bot gets too close to an object.
+
+	#Variables to hold the final calculation:
 	self._X = 0
 	self._Y = 0
-	self._FORCE = 0 
-	self._ANGLE = 0
+	self._Magnitude = 0
+	self._Angle = 0
+
+
+    def set_goal(x_goal, y_goal):
+	self._goal_angle = 360 		#Initialize the angle 
+	self._x_goal = x_goal
+	self._y_goal = y_goal
 	
-
-
-    def calculate_Force(x_curr, y_curr, yaw, laser_scan[])
+	self._deltax_goal = 0
+	self._deltay_goal = 0
 	
-	#First calculate all the components for the diferent foces:
-	self.attracting_force(x_curr, y_curr)
-	self.repelling_forces(x_curr, y_curr, yaw, laser_scan[])
-	
-	#Add up all the forces:
-	self._X = self._deltax_goal
-	self._Y = self._deltay_goal
-
-	comps = len(self._deltax_components)
-	for x (0, comps)
-	    self._X += self._deltax_components[x]
-	    self._Y += self._deltay_components[x]
-	
-
-
-    del force()
-	self._FORCE = round(math.sqrt(math.pow(self._X,2) + math.pow(self._Y, 2)),4)
-	return self._FORCE
-
-
-    del force_angle()
-	
-	return self._ANGLE
-	
-
-
-    #Calculates for the goal: distance from current position, angle and final force from current position
-    def attracting_force(x_curr, y_curr):	
-	
-	self._deltax_goal = self._x_goal - x_curr
-	self._deltay_goal = self-_y_goal - y_curr
-
-	distance = round(Math.sqrt( pow(x_axis,2) + pow(y_axis,2) ) , 4) 	#Distance to the goal
-	self._goal_angle = round(Math.atan2(self._y_axis,self._x_axis),4) 	#Angle in radians
-	
-	if distance < self._goal_radius:		#If the robot is IN the goal, the force will be zero
-	    self._x_goal = self._y_goal = 0
-	else if distance < self._goal_threshold 	#If the robot is within the affected area set high attraction
-	    #Multiply by a big number to get the attraction
-	    self._deltax_goal = 300 * (distance - self._goal_radius) * 	math.cos(self._goal_angle)		 
-	    self._deltay_goal = 300 * (distance - self._goal_radius) * 	math.sin(self._goal_angle)
-	else 						#If the robot is to far from the goal, set force very high:
-	    self._deltax_goal = 500 * (distance - self._goal_radius) * 	math.cos(self._goal_angle)		 
-	    self._deltay_goal = 500 * (distance - self._goal_radius) * 	math.sin(self._goal_angle)
+	self._goal_threshhold = 20 	#The attracting force will be grater outside the threshhod
+	self._goal_radius = 2
 
 
 
-    #Calculates for each object: Angle, x_position and y_position
+    #Calculates the distance to the goal from current position
+    def delta_goal(x_position, y_position):	
+	self._deltax_goal = self._x_goal - x_position
+	self._deltay_goal = self-_y_goal - y_position
+
+
+    #Calculates for each object: x_position and y_position + magnitude despending on the distance
     #
-    def repelling_forces(x_curr, y_curr, yaw, laser_scan[]):    
+    #For each object, the resultant force will be at a 180 deg away.
+    #The values returnes indicate the forces given by the objects.
+    def calculate_repulsive_forces(x_curr, y_curr, yaw, angles_array[], objects_array[]):    
+	self._angles = angles_array		#Contains the angles in degrees
+
+	measeurements = len(angles_array[]) -1 	#Take -1 to make up fot the 0 deg angle reading
 	
-	measeurements = len(laser_scan[]) -1 	#Take -1 to make up fot the 0 angle reading
-	angle_change = 180 / measurements
-	angle = 0
+	for x in range(0, measurements):
+	    angle = math.radians(self._angles[x])
+	    #Calculate the position of the components relative to initial state of the bot:
+	    self._x_components[x] = x_curr + (objects_array[x] * math.cos(yaw + angle))
+	    self._y_components[x] = y_curr + (objects_array[x] * math.sin(yaw + angle))
 
-	x_tot = 0.0
-	y_tot = 0.0
-
-	for x (0, measurements)
-	    self._forces_angle[x] = math.radians(angle)
-	    #Calculate the position of the components:
-	    self._x_components[x] = x_curr + (laser_scan[x] * math.cos(yaw + self._forces_angle[x]))
-	    self._y_components[x] = y_curr + (laser_scan[x] * math.sin(yaw + self._forces_angle[x]))
-
-	    if laser_scan[x] < self._obj_min_distance:   #If the robot is to close to the object the force will be higher.
-		self._deltax_components[x] = (-500) * math.cos(self._forces_angle[x])
-		self._deltay_components[x] = (-500) * math.sin(self._forces_angle[x])
+	    if objects_array[x] < self._obj_min_distance:   #If the robot is to close to the object the force will be high.
+		self._dx_components[x] = (-200) * (self._x_components[x]) * math.cos(angle)
+		self._dy_components[x] = (-200) * math.sin(angle)
 	    
-	    else if laser_scan[x] < self._objs_threshhold:	#If the robot is inside the theshold.Set a scaling force
-		self._deltax_components[x] = (-30) * (self._x_components[x] - self._obj_min_distance) * math.cos(self._forces_angle[x])
-		self._deltay_components[x] = (-30) * (self._x_components[x] - self._obj_min_distance) * math.sin(self._forces_angle[x])
+	    else if objects_array[x] < self._obj_effect_radius:	#If the robot is inside the theshhold.Set a scaling force
+		self._dx_components[x] = (-30) * (self._x_components[x] - self._obj_min_distance) * math.cos(angle)
+		self._dy_components[x] = (-30) * (self._y_components[x] - self._obj_min_distance) * math.sin(angle)
 
 	    else		#If the robot is far from the object it wont be affected by it
-		self._deltax_components[x] = 0
-		self._deltay_components[x] = 0
+		self._dx_components[x] = 0
+		self._dy_components[x] = 0
+	    
 
 
-
-
+    #The bot will recieve "repulsive forces" from objects and move away from them, without a goal in particular.
+    def forces_without_goal(x_curr, y_curr, yaw, angles_array[], objects_array[])
+      
+      calculate_repulsive_forces(x_curr, y_curr, yaw, angles_array[], objects_array[])
+      
+      for x in range(0, len(self._angles)-1):
+	self._X += self._dx_components[x]
+	self._Y += self._dy_components[x]
 
