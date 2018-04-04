921e3dfe9e1185804c9f5bd1e5b25e023cee2cc9
Cristina Rivera <33240525+clr3@users.noreply.github.com>
2018-04-04 13:17:04 +0000

    Update laser_subscriber.py

--- scripts/laser_subscriber.py
+++ scripts/laser_subscriber.py
@@ -9,7 +9,7 @@ from sensor_msgs.msg import LaserScan
 # 
 # To change the amount of readings just edit: self._angles_array
 #
-# For the robot in gazebo:
+# For my turtlebot in gazebo:
 # angle_min: -1.57079994678
 # angle_max: 1.57079994678
 # angle_increment: 0.00436940183863.
@@ -17,20 +17,26 @@ from sensor_msgs.msg import LaserScan
 #
 
 class LaserScanReader():
-    def __init__(self, x_goal, y_goal, topic_name = '/scan'):		#'/scan' is the topic that publisher LaserScan
-        self._topic_name = topic_name
-        self._sub = rospy.Subscriber(self._topic_name, LaserScan, self.topic_callback(msg))
+    def __init__(self, topic_name = '/scan'):		#'/scan' is the topic that publisher LaserScan
+        
+	self._topic_name = topic_name
+        self._sub = rospy.Subscriber(self._topic_name, LaserScan, self.topic_callback(msg)) 
+
+	###self._scanner_arrary = msg.ranges
+	#WThis array will hold values for every 10 deg:
+	#Degrees are measured from the right, anticlockwise
+	self._angles_array = [0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180]
+	# number of angles we want -1:	
+        self._no_of_measurements = 18	
+ 	self._objects_array = 0 	#Empty array tho hold the objects
+					
+        
+    def set_goal(x_goal, y_goal):        
 	self._threshold = 1		
 	self._x_goal = x_goal
 	self._y_goal = y_goal
-	#Initialize an ampty array to copy LaserScan.ranges[]
-	self._scanner_arrary = msg.ranges
-	#This array will hold values for every 10 deg:
-	#Degrees are measured from the right, anticlockwise
-	self._angles_array = [0,30,60,90,120,150,180]	
-	self._scans = []
-					
-        
+
+	
     def topic_callback(self, msg):
 	#Make a copy of laser array:
 	self._scanner_arrary = msg.ranges
@@ -39,20 +45,21 @@ class LaserScanReader():
  	return 
 	
 
-    #Will return an array wich contains laser scan values at various degrees (this will be the distance from the object
-    def create_angles_array(self):
+    #Will create an array wich containt laser scan values at various degrees
+    def create_objects_array(self):
+      int read_now = 0		#Initialize the angles count
+      int tick = self._arr_size / self._no_of_measurements
+      for x in range(0, self._no_of_measuremets):
+	if(x == read_now):
+	  self._objects_array.append(self._scanner_array[x])
+  	  i += tick
 
-	#How many angles will be checked depending in the _angles_array length:
-	measuerements = len(self._angles_array)	-1 	#Take -1 because of the angle at 0 degrees
+    def get_angles_array():
+      return self._angles_array
 
-	read = self._arra_size / measurements 
-	read_now = 0
-
-	for x (0, measuerements)
-	    self._scans = [self.scanner_array[read_now]]
-	    read_now += read
-
-	return self._scans
+    def get_objects_array():
+      create_objects_array(self)
+      return self._objects_array
 
 
     def shutdownhook():
