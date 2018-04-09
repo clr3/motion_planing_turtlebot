dd93ab93072405bce973196e61b6e0f5f0939ffe
Cristina Rivera <33240525+clr3@users.noreply.github.com>
2018-04-09 13:47:37 +0000

    Update laser_subscriber.py

--- scripts/laser_subscriber.py
+++ scripts/laser_subscriber.py
@@ -13,68 +13,93 @@ from sensor_msgs.msg import LaserScan
 # range_max = 30.0
 #
 
+arr_size = 0
+_scanner_array = []
+#This array will hold values for every 10 deg:
+#Degrees are measured from the right, anticlockwise
+angles_array = [0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180]
+# number of angles we want :	
+_no_of_measurements = 19
+objects_array = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+
 class LaserScanReader():
-    def __init__(self, topic_name = '/scan'):		
 
-        self._topic_name = topic_name
-        self._sub = rospy.Subscriber(self._topic_name, LaserScan, self.topic_callback(msg)) 
+    def __init__(self, topic_name = '/scan'):	
+      	self._topic_name = topic_name
+      	self._sub = rospy.Subscriber('/scan', LaserScan, self.laser_callback) 
+    	
 
-	###self._scanner_arrary = msg.ranges
-	#WThis array will hold values for every 10 deg:
-	#Degrees are measured from the right, anticlockwise
-	self._angles_array = [0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180]
-	# number of angles we want -1:	
-        self._no_of_measurements = 18	
- 	self._objects_array = 0 	#Empty array tho hold the objects
-
-    def set_goal(x_goal, y_goal):        
-	self._threshold = 1		
-	self._x_goal = x_goal
-	self._y_goal = y_goal
-
-    def topic_callback(self, msg):
-	#Make a copy of laser array:
-	self._scanner_arrary = msg.ranges
-	#Check range of laser array:
-	self._arr_size = len(self._scanner_array)
- 	return 
+    def laser_callback(self, msg):	#Recieves LaserScan Data
+      global arr_size
+      global _scanner_array
+      #Make a copy of laser array:
+      _scanner_array = msg.ranges
+      #Check range of laser array:
+      arr_size = len(_scanner_array)
+      self.create_objects_array()
+      return 
 	
-	#Will create an array wich containt laser scan values at various degrees
+
+	#Will create an array wich contains laser scan values at various degrees
     def create_objects_array(self):
-      int read_now = 0		#Initialize the angles count
-      int tick = self._arr_size / self._no_of_measurements
-      for x in range(0, self._no_of_measuremets):
+      global arr_size
+      global _scanner_array
+      global _no_of_measurements
+
+      read_now = 0		#Initialize the angles count
+      tick = arr_size / (_no_of_measurements-1)
+      count = 0
+
+      for x in range(0, arr_size+1):
 	if(x == read_now):
-	  self._objects_array.append(self._scanner_array[x])
-  	  i += tick
+	  obj = _scanner_array[x]
+	  objects_array[count] = obj 
+	  count += 1 
 
-    def get_angles_array():
-      return self._angles_array
+	  if read_now == 40:	
+	    read_now = 39
+          read_now += tick
 
-    def get_objects_array():
-      create_objects_array(self)
-      return self._objects_array
 
+    def get_angles_array(self):
+      global angles_array
+      ang = angles_array
+      return ang
+
+
+    def get_objects_array(self):
+      global objects_array
+      objs = objects_array
+      return objs
+
+
+
+if __name__ == "__main__":
+   
+    laserscan_sub_object = LaserScanReader()	
+
+    rospy.init_node('test_laserscan_sub_node')		#Creating the node 
+    rate = rospy.Rate(0.2)
+
+    ctrl_c = False
     def shutdownhook():
         global ctrl_c
         print "shutdown time!"
         ctrl_c = True
 
-
-
-
-
-if __name__ == "__main__":
-    rospy.init_node('laserscan_sub_node')		#Creating the node
-    laserscan_sub_object = LaserScanReader()	#Initialize with the values for the goal
-    rospy.loginfo(laserscan_sub_object.topic_callback())  #Log 
-    rate = rospy.Rate(0.5)
-
-    ctrl_c = False
-
-    rospy.on_shutdown(laserscan_sub_object.shutdownhook())
+    rospy.on_shutdown(shutdownhook)
     
-    while not ctrl_c:				
-        data = laserscan_sub_object.moving_direction()
-        rospy.loginfo(data)
+    while not ctrl_c:			
+	
+	objs_array = laserscan_sub_object.get_objects_array()
+	angle_array = laserscan_sub_object.get_angles_array()
+	length = len(angle_array)
+
+        rospy.loginfo('Objects: {:.4f} '.format(length))
+   	for x in range(0, length):
+	  ang = angle_array[x]
+	  dist = objs_array[x]
+	  rospy.loginfo('[ {:.2f} ] -> {:.2f} '.format(ang, dist))
+
         rate.sleep()
